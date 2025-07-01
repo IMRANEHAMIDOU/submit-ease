526c33fe73bf249fe2d520578521febc7a4e0133
Abdoul Wahabou Boureima Souley <boureimasouleyabdoulwahabou@gmail.com>
2025-07-01 19:20:05 +0200

    Ajout du controller compaingn

--- submit-ease-api/app/controllers/compaingns_controller.rb
+++ submit-ease-api/app/controllers/compaingns_controller.rb
@@ -1,2 +1,42 @@
 class CompaingnsController < ApplicationController
+    # Voir tous les concours
+    def index
+        @compaingns = compaingn.all
+        render json: @compaigns
+    end
+
+    # Voir un concours en particulier
+    def show
+        @compaingns = compaingn.find(params[:id])
+        render json: @compaingn
+    end
+
+    # Créer un nouveau concours
+    def create
+        @compaingn = compaingn.new(compaingn.params)
+
+        if @compaingn.save
+            render json: @compaingn, status: :created
+        else
+            render json: @compaingn.errors, status: :unprocessable_entity
+        end
+    end
+
+    # Modifier un concours
+    def update
+        @compaingn = compaingn.find(params[:id])
+
+        if @compaingn.update(compaingn.params)
+            render json: @compaingn
+        else
+            render json: @compaingn.errors, status: :unprocessable_entity
+        end
+    end
+
+    # Supprimer un concours
+    def destroy
+        @compaingn = compaingn.find(params[:id])
+        @compaingn.destroy
+        head :no_content
+    end
 end
