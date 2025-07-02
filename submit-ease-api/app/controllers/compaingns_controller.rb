f09a97219da36a4342effbe8211853b037350787
Abdoul Wahabou Boureima Souley <boureimasouleyabdoulwahabou@gmail.com>
2025-07-02 12:45:34 +0200

    ajout de mon fichier compaingns_controller

--- submit-ease-api/app/controllers/compaingns_controller.rb
+++ submit-ease-api/app/controllers/compaingns_controller.rb
@@ -1,55 +1,55 @@
 class CompaingnsController < ApplicationController
-
-  # Voir la liste des concours
-  def index
-    @compaingns = Compaingn.all
-    render json: @compaingns
-  end
-
-  # Voir un concours en particulier
-  def show
-    @compaingn = Compaingn.find(params[:id])
-    render json: @compaingn
-  end
-
-  # Créer un nouveau concours
-  def create
-    @compaingn = Compaingn.new(compaingn_params)
-
-    if @compaingn.save
-      render json: @compaingn, status: :created
-    else
-      render json: @compaingn.errors, status: :unprocessable_entity
+    # Voir la liste des concours
+    def index
+        @compaingns = Compaingn.all
+        render json: @compaingns
     end
-  end
 
-  # Modifier un concours
-  def update
-    @compaingn = Compaingn.find(params[:id])
-
-    if @compaingn.update(compaingn_params)
-      render json: @compaingn
-    else
-      render json: @compaingn.errors, status: :unprocessable_entity
+    # Voir un concours en particulier
+    def show
+        @compaingn = Compaingn.find(params[:id])
+        render json: @compaingn
     end
-  end
 
-  # Supprimer un concours
-  def destroy
-    @compaingn = Compaingn.find(params[:id])
-    @compaingn.destroy
-    head :no_content
-  end
+    # Créer un nouveau concours
+    def create
+        @compaingn = Compaingn.new(compaingn_params)
 
-  # Méthodes privées
-  private
+        if @compaingn.save
+        render json: @compaingn, status: :created
+        else
+        render json: @compaingn.errors, status: :unprocessable_entity
+        end
+    end
 
-  def compaingn_params
-    params.require(:compaingn).permit(
-      :nom, :description, :test_ecris, :entretien,
-      :date_ouverture, :date_cloture,
-      :nbre_dossier_limité, :nombre_dossier,
-      :status, :lien_publication, :organisation_id
-    )
-  end
+    # Modifier un concours
+    def update
+        @compaingn = Compaingn.find(params[:id])
+
+        if @compaingn.update(compaingn_params)
+        render json: @compaingn
+        else
+        render json: @compaingn.errors, status: :unprocessable_entity
+        end
+    end
+
+    # Supprimer un concours
+    def destroy
+        @compaingn = Compaingn.find(params[:id])
+        @compaingn.destroy
+        head :no_content
+    end
+
+    # Les methodes invisibles par l'utilisateur
+    private
+
+    # Proteger les données recues
+    def compaingn_params
+        params.require(:compaingn).permit(
+        :nom, :description, :test_ecris, :entretien,
+        :date_ouverture, :date_cloture,
+        :nbre_dossier_limité, :nombre_dossier,
+        :status, :lien_publication, :organisation_id
+        )
+    end
 end
