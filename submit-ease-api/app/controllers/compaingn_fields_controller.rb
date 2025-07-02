00f04b744e465761d084566ca7fa2bba2faae7d0
Abdoul Wahabou Boureima Souley <boureimasouleyabdoulwahabou@gmail.com>
2025-07-02 11:47:11 +0200

    modification de mon fichier compaingns_controller

--- submit-ease-api/app/controllers/compaingn_fields_controller.rb
+++ submit-ease-api/app/controllers/compaingn_fields_controller.rb
@@ -1,2 +1,42 @@
 class CompaingnFieldsController < ApplicationController
+    # Afficher tous les champs existants
+    def index
+        @fields = compaingnField.all
+        render json: @fields
+    end
+
+    # Afficher un champ précis à partir de son ID
+    def show
+        @field = compaingnField.find(params[:id])
+        render json: @field
+    end
+
+    # Créer un nouveau champ
+    def create
+        @field = compaingnField.new(compaingn_field_params)
+        if @field.save
+            render json: @field, status: :created
+        else
+            render json: @field.errors, status: :unprocessable_entity
+        end
+    end
+
+    # Mettre à jour un champ existant
+    def update
+        @field = compaingnField.find(params[:id])
+        if @field.update(compaingn_field_params)
+            render json: @field
+        else
+            render json: @field.errors, status: :unprocessable_entity
+        end
+    end
+
+    # Supprimer un champ existant
+    def destroy
+        @field = compaingnField.find(params[:id])
+        @field.destroy
+        head :no_content
+    end
+
+    
 end
--- submit-ease-api/app/controllers/compaingns_controller.rb
+++ submit-ease-api/app/controllers/compaingns_controller.rb
@@ -1,55 +1,55 @@
 class CompaingnsController < ApplicationController
-    # Voir la liste des concours
-    def index
-        @compaingns = compaingn.all
-        render json: @compaigns
+
+  # Voir la liste des concours
+  def index
+    @compaingns = Compaingn.all
+    render json: @compaingns
+  end
+
+  # Voir un concours en particulier
+  def show
+    @compaingn = Compaingn.find(params[:id])
+    render json: @compaingn
+  end
+
+  # Créer un nouveau concours
+  def create
+    @compaingn = Compaingn.new(compaingn_params)
+
+    if @compaingn.save
+      render json: @compaingn, status: :created
+    else
+      render json: @compaingn.errors, status: :unprocessable_entity
     end
+  end
 
-    # Voir un concours en particulier
-    def show
-        @compaingns = compaingn.find(params[:id])
-        render json: @compaingn
+  # Modifier un concours
+  def update
+    @compaingn = Compaingn.find(params[:id])
+
+    if @compaingn.update(compaingn_params)
+      render json: @compaingn
+    else
+      render json: @compaingn.errors, status: :unprocessable_entity
     end
+  end
 
-    # Créer un nouveau concours
-    def create
-        @compaingn = compaingn.new(compaingn.params)
+  # Supprimer un concours
+  def destroy
+    @compaingn = Compaingn.find(params[:id])
+    @compaingn.destroy
+    head :no_content
+  end
 
-        if @compaingn.save
-            render json: @compaingn, status: :created
-        else
-            render json: @compaingn.errors, status: :unprocessable_entity
-        end
-    end
+  # Méthodes privées
+  private
 
-    # Modifier un concours
-    def update
-        @compaingn = compaingn.find(params[:id])
-
-        if @compaingn.update(compaingn.params)
-            render json: @compaingn
-        else
-            render json: @compaingn.errors, status: :unprocessable_entity
-        end
-    end
-
-    # Supprimer un concours
-    def destroy
-        @compaingn = compaingn.find(params[:id])
-        @compaingn.destroy
-        head :no_content
-    end
-
-    # Les methodes invisibles par l'utilisateur
-    private
-
-    # Proteger les données recues 
-    def compaingn_params
-        params.require(:compaingn).permit(
-        :nom, :description, :test_ecris, :entretien,
-        :date_ouverture, :date_cloture,
-        :nbre_dossier_limité, :nombre_dossier,
-        :status, :lien_publication, :organisation_id
-        )
-    end
+  def compaingn_params
+    params.require(:compaingn).permit(
+      :nom, :description, :test_ecris, :entretien,
+      :date_ouverture, :date_cloture,
+      :nbre_dossier_limité, :nombre_dossier,
+      :status, :lien_publication, :organisation_id
+    )
+  end
 end
